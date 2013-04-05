377327e3fa77da3c6ba282ff79f4a1ba7d20a24a
Joel Shin <jwshin@gmail.com>
2013-04-05 11:05:58 -0700

    cleandup

---
+++ lib/ripper_manager.rb
@@ -0,0 +1,32 @@
+require 'log_ripper'
+
+class RipperManager
+
+ def initialize( pattern_file )
+  @r = LogRipper.new
+  process( pattern_file ) 
+ end
+
+ def run( log_file )
+  @r.run( log_file )
+ end
+
+ def process( pattern_file )
+  File.open( pattern_file ) do |f|
+   until f.eof?
+    process_pattern( f.readline ) 
+   end
+  end
+ end
+
+ def process_pattern( pattern ) 
+  return if pattern.empty? || pattern =~ /^#/
+  #raise "Expected print <xpath>" unless tokens.size == 2
+  #@r.on_path( Regexp.new(pattern.chomp) ) { |e| puts "---> ohoh: #{e}" }
+  @r.on_path( /#{pattern.chomp}/ ) { |e| puts e }
+ end
+
+end
+  
+   
+   
--- main_2.rb
+++ main_2.rb
@@ -1,16 +1,9 @@
 $LOAD_PATH << File.expand_path('../lib', __FILE__)
 require 'log_file'
-require 'ez_ripper'
+require 'ripper_manager'
 
 puts '---> Start <----------------------------------------------------------------'
 
-=begin
-lr = LogRipper.new do
- on_path( /SQL/ ) { |t| puts "---> gaga: #{t}" }
-end
-lr.run( 'development.log' )
-=end
-
-EzRipper.new( 'pattern.ezr' ).run( 'development.log' )
+RipperManager.new( 'pattern.ezr' ).run( 'development.log' )
 
 puts '---> End <------------------------------------------------------------------'
